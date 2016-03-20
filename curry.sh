4085fcade2bfa8dfc7addf8c65247d60460b160f
Ifedapo Olarewaju <ifedapoolarewaju@gmail.com>
2016-03-20 11:47:35 +0100

    alias commands

---
+++ curry.sh
@@ -0,0 +1,39 @@
+alias ll='ls -la'
+
+alias aliases='cat ~/alias_file'
+
+alias djmg='python manage.py migrate'
+alias djmkg='python manage.py makemigrations'
+alias djru='python manage.py runserver'
+
+alias 2u='cd ~/projects/2u'
+alias leadform='cd ~/projects/2u/lead-forms'
+alias PW='cd ~/projects/photows'
+alias PWC='cd ~/projects/photows-client'
+alias harp='cd ~/projects/2u/harp'
+alias balut='workon balut'
+alias pages='workon pages'
+alias venhop='workon venhop'
+alias subman='workon subman'
+alias adscout='cd ~/projects/adscout-admin'
+
+alias pipf='pip freeze > requirements.txt'
+alias pipi='pip install -r requirements.txt'
+
+alias gs='git status '
+alias ga='git add '
+alias gb='git branch '
+alias gc='git commit'
+alias gd='git diff'
+alias gch='git checkout '
+alias gk='gitk --all&'
+alias gx='gitx --all'
+alias gp='git push'
+alias gpl='git pull'
+alias gl='git log'
+alias gf='git fetch'
+alias dp='cd ~/django-projects'
+alias mp='cd ~/mean-projects'
+
+alias pgstart='pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start'
+
